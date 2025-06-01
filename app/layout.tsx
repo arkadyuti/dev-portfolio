06923f0f62978d4dd7bbc992fd09dd3451df9ef7
Arkadyuti Sarkar <asarkar@tekion.com>
2025-06-01 11:39:36 +0530

    Google's site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,6 +121,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+        <meta name="google-site-verification" content="Q_goUodclCKZr38sZFhH2UIJE3hEm3miydrju53Y1Pw" />
       </head>
       <body>
         <AuthProvider>
