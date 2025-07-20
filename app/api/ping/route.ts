4df3089da5a7fa858ca57987c8d65e00fef5ba52
Arkadyuti Sarkar <arkadooti.sarkar@gmail.com>
2025-07-20 21:45:16 +0530

    added ping api

---
+++ app/api/ping/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server'
+import packageJson from '../../../package.json'
+
+export async function GET() {
+  return NextResponse.json({
+    success: true,
+    message: 'pong',
+    version: packageJson.version,
+    timestamp: new Date().toISOString()
+  })
+}
